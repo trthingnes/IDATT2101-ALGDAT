2bb331f32e7602c1fff08e3691e4e222902442b3
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-10-06 10:44:23 +0200

    Use StringTokenizer

--- src/main/kotlin/graphs/GraphReader.kt
+++ src/main/kotlin/graphs/GraphReader.kt
@@ -1,19 +1,21 @@
 package graphs
 
 import java.io.File
+import java.util.*
 
 class GraphReader {
     fun read(path : String) : Graph {
         val lines = File(path).readLines().iterator()
 
         // * Gets number of nodes and edges from file header.
-        val sizes = lines.next().split(" ")
-        val n = sizes.first().toInt()
+        val sizes = StringTokenizer(lines.next()).toList()
+        val n = (sizes.first() as String).toInt()
+        val e = (sizes.last() as String).toInt()
 
         // * Makes an array of edges. Example: [[1, 3], [1, 5], [3, 5]].
-        val edges = Array(sizes.last().toInt()) {
-            val nodes = lines.next().split(" ")
-            Array(2) { nodes[it].toInt() }
+        val edges = Array(e) {
+            val nodes = StringTokenizer(lines.next()).toList()
+            Array(2) { (nodes[it] as String).toInt() }
         }
 
         return Graph(n, edges)
